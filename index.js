acdf7c71d583735a5eeb19b9df8b9832720fb889
Ycenta <thorbenliya@gmail.com>
2023-01-29 00:44:47 +0100

    test deviceorientation api

--- index.js
+++ index.js
@@ -154,3 +154,52 @@ document.addEventListener("click", (e) => {
         appEngine.closeApplication();
     }
 });
+
+
+// random BS TBD
+
+function handleOrientation(event) {
+    const absolute = event.absolute;
+    const alpha = event.alpha;
+    const beta = event.beta;
+    const gamma = event.gamma;
+  
+    // Do stuff with the new orientation data
+}
+
+
+const ball = document.querySelector(".ball");
+const garden = document.querySelector(".garden");
+const output = document.querySelector(".output");
+
+const maxX = garden.clientWidth - ball.clientWidth;
+const maxY = garden.clientHeight - ball.clientHeight;
+
+function handleOrientation(event) {
+  let x = event.beta; // In degree in the range [-180,180)
+  let y = event.gamma; // In degree in the range [-90,90)
+
+  output.textContent = `beta : ${x}\n`;
+  output.textContent += `gamma: ${y}\n`;
+
+  // Because we don't want to have the device upside down
+  // We constrain the x value to the range [-90,90]
+  if (x > 90) {
+    x = 90;
+  }
+  if (x < -90) {
+    x = -90;
+  }
+
+  // To make computation easier we shift the range of
+  // x and y to [0,180]
+  x += 90;
+  y += 90;
+
+  // 10 is half the size of the ball
+  // It center the positioning point to the center of the ball
+  ball.style.top = `${(maxY * y) / 180 - 10}px`;
+  ball.style.left = `${(maxX * x) / 180 - 10}px`;
+}
+
+window.addEventListener("deviceorientation", handleOrientation);
