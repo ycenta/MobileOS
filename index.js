13f9c4a2122e87f92778d9554c34f09f090f9504
Ycenta <thorbenliya@gmail.com>
2023-01-30 00:26:29 +0100

    tiny update

--- index.js
+++ index.js
@@ -168,12 +168,8 @@ function handleOrientation(event) {
 }
 
 
-const ball = document.querySelector(".ball");
-const garden = document.querySelector(".garden");
-const output = document.querySelector(".output");
 
-const maxX = garden.clientWidth - ball.clientWidth;
-const maxY = garden.clientHeight - ball.clientHeight;
+const output = document.querySelector(".output");
 
 function handleOrientation(event) {
   let x = event.beta; // In degree in the range [-180,180)
