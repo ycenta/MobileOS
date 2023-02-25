b3303b6467f066873721ac26249834f564529c94
Samuel GUENIER <guenier.samuel@gmail.com>
2023-02-25 18:23:24 +0100

    Better style on calculator, del function and keypad usage

--- Apps/calculator.js
+++ Apps/calculator.js
@@ -15,6 +15,7 @@ const calculator = {
           const btn_calc = document.querySelectorAll('.btn-calc');
           const btn_clear = document.getElementById('btn-clear');
           const btn_proceed = document.getElementById('btn-proceed');
+          const btn_del = document.getElementById('btn-del');
 
           btn_calc.forEach((btn) => {
             btn.addEventListener('click', (event) => {
@@ -67,6 +68,61 @@ const calculator = {
               alert("Invalid Calculation");
             }
           });
+
+          document.addEventListener("keyup", function(event) {
+
+            let tmp_tab = ["+","-","*","/"];
+
+            if ( !isNaN(event.key) ) {
+              result.value += event.key;
+            } else if ( event.key == "Delete" ) {
+              result.value = "";
+            } else if ( event.key == "(" || event.key == ")" ) {
+              if ( event.key == "(" && isNaN(result.value.charAt(result.value.length-1)) ) {
+                result.value += event.key;
+              } else if ( event.key == ")" ) {
+                let count = (result.value.match(/\(/g) || []).length;
+                let countbis = (result.value.match(/\)/g) || []).length;
+                
+                if ( countbis < count && (!isNaN(result.value.charAt(result.value.length-1)) || result.value=="") ) {
+                  result.value += event.key;
+                }
+              }
+            } else if ( tmp_tab.includes(event.key) ) {
+              if ( (isNaN(result.value.charAt(result.value.length-1)) && result.value.charAt(result.value.length-1) != ")") ) {
+                return;
+              }
+
+              result.value += tmp_tab[tmp_tab.indexOf(event.key)];
+            } else if ( event.key == "Enter" ) {
+              if ( isNaN(result.value.charAt(result.value.length-1)) ) {
+                result.value = result.value.substring(0, result.value.length-1);
+              }
+  
+              let count = (result.value.match(/\(/g) || []).length;
+              let countbis = (result.value.match(/\)/g) || []).length;
+  
+              if ( countbis<count ) {
+                for (let i = 0; i < count-countbis; i++) {
+                  result.value += ")";
+                }
+              }
+  
+              try {
+                result.value = eval(result.value);
+              } catch (error) {
+                alert("Invalid Calculation");
+              }
+            } else if ( event.key == "Backspace"  ) {
+              result.value = result.value.substring(0, result.value.length-1);
+            }
+          });
+
+          btn_del.addEventListener('click', (event) => {
+            result.value = result.value.substring(0, result.value.length-1);
+          });
+
+          html
         })
         .catch(error => alert("Erreur dans le chargement de l'application."));
     },
