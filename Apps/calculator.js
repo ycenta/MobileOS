e04109420929d09b57014e2dc4c4d60688bce457
Samuel GUENIER <guenier.samuel@gmail.com>
2023-02-25 17:51:59 +0100

    Adding new manner of inserting app html and better style on calculator app

--- Apps/calculator.js
+++ Apps/calculator.js
@@ -2,99 +2,73 @@ const calculator = {
     message: "Hello from myModule!",
     loadApp()
     {
-      // alert("load app");
-      const htmlParent = document.getElementById("app-overlay-id");
-      let html = `<div>Hello from calculator<br><div>
-      <input type="text" id="result" disabled>
-    </div>
-    <div>
-      <button id="btn-clear">C</button>
-      <button class="btn-calc" value="(">(</button>
-      <button class="btn-calc" value=")">)</button>
-      <button class="btn-calc" value="/">/</button>
-    </div>
-    <div>
-      <button class="btn-calc" value="7">7</button>
-      <button class="btn-calc" value="8">8</button>
-      <button class="btn-calc" value="9">9</button>
-      <button class="btn-calc" value="*">*</button>
-    </div>
-    <div>
-      <button class="btn-calc" value="4">4</button>
-      <button class="btn-calc" value="5">5</button>
-      <button class="btn-calc" value="6">6</button>
-      <button class="btn-calc" value="-">-</button>
-    </div>
-    <div>
-      <button class="btn-calc" value="1">1</button>
-      <button class="btn-calc" value="2">2</button>
-      <button class="btn-calc" value="3">3</button>
-      <button class="btn-calc" value="+">+</button>
-    </div>
-    <div>
-      <button class="btn-calc" value="0">0</button>
-      <button class="btn-calc" value=".">.</button>
-      <button id="btn-proceed">=</button>
-    </div></div>`;
-      htmlParent.insertAdjacentHTML('beforeend', html);
+      // const htmlParent = document.getElementById("app-overlay-id");
 
-      let result = document.getElementById("result");
+      fetch('../HtmlTemplates/calculator.html')
+        .then(response => response.text())
+        .then(html => {
+          const htmlParentbis = document.getElementById("app-overlay-id");
+          htmlParentbis.insertAdjacentHTML('beforeend', html);
 
-      const btn_calc = document.querySelectorAll('.btn-calc');
-      const btn_clear = document.getElementById('btn-clear');
-      const btn_proceed = document.getElementById('btn-proceed');
+          let result = document.getElementById("result");
 
-      btn_calc.forEach((btn) => {
-        btn.addEventListener('click', (event) => {
-          let tmp = event.target.value;
+          const btn_calc = document.querySelectorAll('.btn-calc');
+          const btn_clear = document.getElementById('btn-clear');
+          const btn_proceed = document.getElementById('btn-proceed');
 
-          if ( isNaN(tmp) ) {
+          btn_calc.forEach((btn) => {
+            btn.addEventListener('click', (event) => {
+              let tmp = event.target.value;
 
-            if ( tmp == "(" ) {
-              result.value += tmp;
-            } else if ( tmp == ")" ) {
-              let count = (result.value.match(/\(/g) || []).length;
-              let countbis = (result.value.match(/\)/g) || []).length;
-              
-              if ( countbis < count && (!isNaN(result.value.charAt(result.value.length-1)) || result.value=="") ) {
+              if ( isNaN(tmp) ) {
+
+                if ( tmp == "(" ) {
+                  result.value += tmp;
+                } else if ( tmp == ")" ) {
+                  let count = (result.value.match(/\(/g) || []).length;
+                  let countbis = (result.value.match(/\)/g) || []).length;
+                  
+                  if ( countbis < count && (!isNaN(result.value.charAt(result.value.length-1)) || result.value=="") ) {
+                    result.value += tmp;
+                  }
+                } else if ( isNaN(result.value.charAt(result.value.length-1)) && result.value.charAt(result.value.length-1) != ")" ) {
+                  return;
+                } else {
+                  result.value += tmp;
+                }
+              } else {
                 result.value += tmp;
               }
-            } else if ( isNaN(result.value.charAt(result.value.length-1)) && result.value.charAt(result.value.length-1) != ")" ) {
-              return;
-            } else {
-              result.value += tmp;
+            });
+          });
+
+          btn_clear.addEventListener('click', (event) => {
+            result.value = "";
+          });
+
+          btn_proceed.addEventListener('click', (event) => {
+
+            if ( isNaN(result.value.charAt(result.value.length-1)) ) {
+              result.value = result.value.substring(0, result.value.length-1);
             }
-          } else {
-            result.value += tmp;
-          }
-        });
-      });
 
-      btn_clear.addEventListener('click', (event) => {
-        result.value = "";
-      });
+            let count = (result.value.match(/\(/g) || []).length;
+            let countbis = (result.value.match(/\)/g) || []).length;
 
-      btn_proceed.addEventListener('click', (event) => {
+            if ( countbis<count ) {
+              for (let i = 0; i < count-countbis; i++) {
+                result.value += ")";
+              }
+            }
 
-        if ( isNaN(result.value.charAt(result.value.length-1)) ) {
-          result.value = result.value.substring(0, result.value.length-1);
-        }
-
-        let count = (result.value.match(/\(/g) || []).length;
-        let countbis = (result.value.match(/\)/g) || []).length;
-
-        if ( countbis<count ) {
-          for (let i = 0; i < count-countbis; i++) {
-            result.value += ")";
-          }
-        }
-
-        try {
-          result.value = eval(result.value);
-        } catch (error) {
-          alert("Invalid Calculation");
-        }
-      });
+            try {
+              result.value = eval(result.value);
+            } catch (error) {
+              alert("Invalid Calculation");
+            }
+          });
+        })
+        .catch(error => alert("Erreur dans le chargement de l'application."));
     },
 
   };
--- Apps/settings.js
+++ Apps/settings.js
@@ -2,78 +2,29 @@ const settings = {
     message: "Hello from myModule!",
     settingsArray: JSON.parse(localStorage.getItem("settingsTopBar")),
     loadApp(){
-      // alert("load app");
-      const htmlParent = document.getElementById("app-overlay-id");
 
-      let html = `<div >Hello from Settings<br>
-      <form action="#" id="settings">
-        <input type="checkbox" id="checkboxNetwork" name="checkboxNetwork">
-        <label for="checkboxNetwork">Latence du réseau</label>
+      fetch('../HtmlTemplates/settings.html')
+        .then(response => response.text())
+        .then(html => {
 
-        <input type="checkbox" id="checkboxNetwork" name="checkboxNetwork">
-        <label for="checkboxNetwork">Latence du réseau</label>
+          const htmlParent = document.getElementById("app-overlay-id");
+          htmlParent.insertAdjacentHTML('beforeend', html);
 
-        <h2>Affichage de l'Heure</h2>
-        <input type="checkbox" id="hours" name="hours">
-        <label for="hours">Heure</label><br>
-        <input type="checkbox" id="minutes" name="minutes">
-        <label for="minutes">Minutes</label><br>
-        <input type="checkbox" id="seconds" name="seconds">
-        <label for="seconds">Secondes</label><br>
+          const form = document.getElementById("settings");
+          form.addEventListener("submit", function(event) {
+            event.preventDefault();
+            const formData = new FormData(form);
+            const entries = formData.entries();
+          });
 
-        <h2>Affichage de la date</h2>
-        <input type="checkbox" id="day" name="day">
-        <label for="day">Jour</label><br>
-        <input type="checkbox" id="month" name="month">
-        <label for="month">Mois</label><br>
-        <input type="checkbox" id="year" name="year">
-        <label for="year">Année</label><br>
+          const inputsSettings = document.querySelectorAll("#settings input");
+          inputsSettings.forEach(input => {
+            // set input to check if the settings already exist in the array (localStorage) and if it's enabled
+            if (settings.settingsArray.find(setting => setting.name === input.name)) {
+              input.checked = settings.settingsArray.find(setting => setting.name === input.name).enabled;
+            }
 
-        <h2>Configuration vibration</h2>
-        <input type="checkbox" id="vibration" name="vibration">
-        <label for="vibration">Vibration</label><br>
-        <input type="checkbox" id="vibrationVisibility" name="vibrationVisibility">
-        <label for="vibration">Affichage de l'état de la vibration</label><br>
-
-        <h2>Configuration de la batterie</h2>
-        <input type="checkbox" id="battery" name="battery">
-        <label for="battery">Batterie</label><br>
-        <input type="checkbox" id="batteryVisibility" name="batteryVisibility">
-        <label for="battery">Affichage de l'état de la batterie</label><br>
-
-        <h2>Configuration de la latence réseau</h2>
-        <label for="network">Nom de domaine du serveur</label>
-        <input type="text" id="network" name="network"><br>
-        <label for="networkDelay">délai de rafraichissement en secondes</label>
-        <input type="number" id="networkDelay" name="networkDelay">
-        <input type="submit" value="Submit">
-      </form>
-    </div>
-    `;
-      htmlParent.insertAdjacentHTML('beforeend', html);
-
-      //CODE DE l'APP
-       const form = document.getElementById("settings");
-       form.addEventListener("submit", function(event) {
-         event.preventDefault();
-         const formData = new FormData(form);
-         const entries = formData.entries();
-         for (const entry of entries) {
-           console.log(entry);
-         }
-       });
-
-      //  Récuperer les changements des inputs pour sauvgarder les settings automatiquement
-        const inputsSettings = document.querySelectorAll("#settings input");
-        inputsSettings.forEach(input => {
-          // set input to check if the settings already exist in the array (localStorage) and if it's enabled
-          if (settings.settingsArray.find(setting => setting.name === input.name)) {
-            input.checked = settings.settingsArray.find(setting => setting.name === input.name).enabled;
-          }
-
-          input.addEventListener("change", function(event) {
-            console.log(event.target.name +" : "+event.target.checked +" "+ event.target.value);
-
+            input.addEventListener("change", function(event) {
               // if the settings already exist in the array, we update it
               if (settings.settingsArray.find(setting => setting.name === event.target.name)) {
                   settings.settingsArray.find(setting => setting.name === event.target.name).enabled = event.target.checked;
@@ -83,12 +34,14 @@ const settings = {
                   name: event.target.name,
                   enabled: event.target.checked
                 });
-          }
-            
-          localStorage.setItem("settingsTopBar", JSON.stringify(settings.settingsArray));
-        }
-      );
-    });
+              }
+              
+              localStorage.setItem("settingsTopBar", JSON.stringify(settings.settingsArray));
+            });
+          });
+
+        })
+        .catch(error => alert("Erreur dans le chargement de l'application."));
   }   
 
   };
