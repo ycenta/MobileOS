45eb606b7c1b0d9fc87d0dfb53eefc2fc817f3e3
Yohan Centanni <thorbenliya@gmail.com>
2023-03-01 00:37:25 +0100

    set network by default
    Signed-off-by: Yohan Centanni <thorbenliya@gmail.com>

--- Apps/beer.js
+++ Apps/beer.js
@@ -12,22 +12,35 @@ const beer = {
             let rotateCompensation = 0;
 
             function beer(event) {
+
                 let x = event.beta; 
                 let y = event.gamma; 
-        
-                x += 90;
-                y += 90;
-        
-                if (y > 100) {
-                    currentLower += 10;
-                    rotateCompensation+= 0.5;
-                }
 
-                rectangle.style.transform = "rotateZ("+(((y+rotateCompensation)/2) -45)+"deg) "+"translateY(" + currentLower + "px)";
-                document.getElementById("angle").innerHTML = "x : " + x + "<br>y : " + y;
+                if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+
+                    if (y > 100) {
+                        currentLower += 10;
+                        rotateCompensation+= 0.5;
+                    }
+
+                    rectangle.style.transform = "rotateZ("+(((y+rotateCompensation)/2) -45)+"deg) "+"translateY(" + currentLower + "px)";
+                
+                  } else {
+
+                    x += 90;
+                    y += 90;
+            
+                    if (y > 100) {
+                        currentLower += 10;
+                        rotateCompensation+= 0.5;
+                    }
+
+                    rectangle.style.transform = "rotateZ("+(((y+rotateCompensation)/2) -45)+"deg) "+"translateY(" + currentLower + "px)";
+                }
+                
+            document.getElementById("angle").innerHTML = "x : " + x + "<br>y : " + y;
                 
                 }
-        
         
                 window.addEventListener("deviceorientation", beer);
 
