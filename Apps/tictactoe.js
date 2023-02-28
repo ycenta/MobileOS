703d2e08bbff75cdda89a81a826358302fff7be8
Samuel GUENIER <guenier.samuel@gmail.com>
2023-02-28 23:01:49 +0100

    Debug tictactoe bis

--- Apps/tictactoe.js
+++ Apps/tictactoe.js
@@ -4,185 +4,186 @@ const settings = {
     loadApp(){
         fetch('../HtmlTemplates/tictactoe.html')
         .then(response => alert(response.text()))
-        // .then(html => {
-        //     const htmlParent = document.getElementById("app-overlay-id");
-        //     htmlParent.insertAdjacentHTML('beforeend', html);
+        .then(html => {
+            
+            const htmlParent = document.getElementById("app-overlay-id");
+            htmlParent.insertAdjacentHTML('beforeend', html);
 
-        //     let board = [];
-        //     let currentPlayer = "X";
-        //     let gameStatus = "Game On";
-        //     let saveScores = document.getElementById('saveScores');
-        //     let resetBoard = document.getElementById('resetBoard');
-        //     let seeScore = document.getElementById('scores_display');
-        //     let customAlert = document.getElementById('alert-tictactoe');
-        //     let resetScore = document.getElementById('resetScore');
+            let board = [];
+            let currentPlayer = "X";
+            let gameStatus = "Game On";
+            let saveScores = document.getElementById('saveScores');
+            let resetBoard = document.getElementById('resetBoard');
+            let seeScore = document.getElementById('scores_display');
+            let customAlert = document.getElementById('alert-tictactoe');
+            let resetScore = document.getElementById('resetScore');
 
-        //     const renderBoard = function()
-        //     {
-        //         let boardElement = document.querySelector(".board");
+            const renderBoard = function()
+            {
+                let boardElement = document.querySelector(".board");
 
-        //         boardElement.innerHTML = "";
-        //         for (let i = 0; i < board.length; i++) {
-        //             let cell = document.createElement("div");
-        //             cell.classList.add("cell");
+                boardElement.innerHTML = "";
+                for (let i = 0; i < board.length; i++) {
+                    let cell = document.createElement("div");
+                    cell.classList.add("cell");
 
-        //             if ( board[i] == "X" ) {
-        //                 cell.style.backgroundImage = "url('/Assets/TicTacToe/cross.png')";
-        //             } else if ( board[i] == "O" ) {
-        //                 console.log('ooooh');
-        //                 cell.style.backgroundImage = "url('/Assets/TicTacToe/circle.png')";
-        //             } else {
-        //                 cell.style.backgroundImage = "";
-        //             }
+                    if ( board[i] == "X" ) {
+                        cell.style.backgroundImage = "url('/Assets/TicTacToe/cross.png')";
+                    } else if ( board[i] == "O" ) {
+                        console.log('ooooh');
+                        cell.style.backgroundImage = "url('/Assets/TicTacToe/circle.png')";
+                    } else {
+                        cell.style.backgroundImage = "";
+                    }
 
-        //             // cell.innerHTML = board[i] || "&nbsp;";
-        //             cell.addEventListener("click", () => handleCellClick(i));
-        //             boardElement.appendChild(cell);
-        //         }
-        //     }
+                    // cell.innerHTML = board[i] || "&nbsp;";
+                    cell.addEventListener("click", () => handleCellClick(i));
+                    boardElement.appendChild(cell);
+                }
+            }
 
-        //     const handleCellClick = function(index) {
-        //         if (gameStatus !== "Game On") {
-        //             return;
-        //         }
-        //         if (board[index]) {
-        //             return;
-        //         }
+            const handleCellClick = function(index) {
+                if (gameStatus !== "Game On") {
+                    return;
+                }
+                if (board[index]) {
+                    return;
+                }
 
-        //         if(localStorage.getItem("vibrationPermission") === "true"){
-        //             navigator.vibrate(100);
-        //             console.log("vibrate");
-        //         }
+                if(localStorage.getItem("vibrationPermission") === "true"){
+                    navigator.vibrate(100);
+                    console.log("vibrate");
+                }
 
-        //         board[index] = currentPlayer;
-        //         renderBoard();
+                board[index] = currentPlayer;
+                renderBoard();
                 
-        //         let tmp_check = checkGameStatus();
+                let tmp_check = checkGameStatus();
 
-        //         if ( tmp_check == "X" || tmp_check == "O" ) {
-        //             showAlert("Le joueur "+tmp_check+" a gagné");
-        //         } else if ( tmp_check == "Tie" ) {
-        //             showAlert("Tie");
-        //         }
+                if ( tmp_check == "X" || tmp_check == "O" ) {
+                    showAlert("Le joueur "+tmp_check+" a gagné");
+                } else if ( tmp_check == "Tie" ) {
+                    showAlert("Tie");
+                }
 
-        //         currentPlayer = currentPlayer === "X" ? "O" : "X";
-        //     }
+                currentPlayer = currentPlayer === "X" ? "O" : "X";
+            }
 
-        //     const checkGameStatus = function() {
-        //         let winningConditions = [
-        //           [0, 1, 2],
-        //           [3, 4, 5],
-        //           [6, 7, 8],
-        //           [0, 3, 6],
-        //           [1, 4, 7],
-        //           [2, 5, 8],
-        //           [0, 4, 8],
-        //           [2, 4, 6],
-        //         ];
-        //         for (let condition of winningConditions) {
-        //           let [a, b, c] = condition;
-        //           if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-        //             gameStatus = `${currentPlayer} wins!`;
+            const checkGameStatus = function() {
+                let winningConditions = [
+                  [0, 1, 2],
+                  [3, 4, 5],
+                  [6, 7, 8],
+                  [0, 3, 6],
+                  [1, 4, 7],
+                  [2, 5, 8],
+                  [0, 4, 8],
+                  [2, 4, 6],
+                ];
+                for (let condition of winningConditions) {
+                  let [a, b, c] = condition;
+                  if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+                    gameStatus = `${currentPlayer} wins!`;
 
-        //             // alert("Joueur "+currentPlayer+" a gagné");
+                    // alert("Joueur "+currentPlayer+" a gagné");
 
-        //             let tmp = parseInt(document.getElementById('score'+currentPlayer).value);
+                    let tmp = parseInt(document.getElementById('score'+currentPlayer).value);
 
-        //             document.getElementById('score'+currentPlayer).value = tmp+1;
-        //             document.getElementById('dscore'+currentPlayer).innerHTML = document.getElementById('score'+currentPlayer).value;
+                    document.getElementById('score'+currentPlayer).value = tmp+1;
+                    document.getElementById('dscore'+currentPlayer).innerHTML = document.getElementById('score'+currentPlayer).value;
 
-        //             return currentPlayer;
-        //           }
-        //         }
-        //         if (!board.includes("")) {
-        //           gameStatus = "Tie!";
-        //           return "Tie";
-        //         }
-        //         return 0;
-        //     }
+                    return currentPlayer;
+                  }
+                }
+                if (!board.includes("")) {
+                  gameStatus = "Tie!";
+                  return "Tie";
+                }
+                return 0;
+            }
 
-        //     const showAlert = function(content="") {
-        //         let alertCust = document.getElementById('alert-tictactoe');
+            const showAlert = function(content="") {
+                let alertCust = document.getElementById('alert-tictactoe');
 
-        //         if (alertCust.classList.contains('hidden')) {
-        //             alertCust.innerHTML = content;
-        //             alertCust.classList.remove('hidden');
-        //         } else {
-        //             alertCust.classList.add('hidden');
-        //             gameStatus = "Game On";
-        //             board = Array(9).fill("");
-        //             renderBoard();
-        //         }
-        //     }
+                if (alertCust.classList.contains('hidden')) {
+                    alertCust.innerHTML = content;
+                    alertCust.classList.remove('hidden');
+                } else {
+                    alertCust.classList.add('hidden');
+                    gameStatus = "Game On";
+                    board = Array(9).fill("");
+                    renderBoard();
+                }
+            }
 
-        //     customAlert.addEventListener("click", showAlert);
+            customAlert.addEventListener("click", showAlert);
 
-        //     saveScores.addEventListener("click", function(){
+            saveScores.addEventListener("click", function(){
 
-        //         let scoreX = document.getElementById('scoreX').value;
-        //         let scoreO = document.getElementById('scoreO').value;
+                let scoreX = document.getElementById('scoreX').value;
+                let scoreO = document.getElementById('scoreO').value;
 
-        //         let json_score = {'X': scoreX, 'O': scoreO};
+                let json_score = {'X': scoreX, 'O': scoreO};
 
-        //         localStorage.setItem("scoreTicTacToe", JSON.stringify(json_score));
+                localStorage.setItem("scoreTicTacToe", JSON.stringify(json_score));
 
-        //         if(localStorage.getItem("vibrationPermission") === "true"){
-        //             navigator.vibrate(100);
-        //             console.log("vibrate");
-        //         }
+                if(localStorage.getItem("vibrationPermission") === "true"){
+                    navigator.vibrate(100);
+                    console.log("vibrate");
+                }
 
-        //         console.log(localStorage);
-        //     });
+                console.log(localStorage);
+            });
 
-        //     resetBoard.addEventListener("click", function(){
+            resetBoard.addEventListener("click", function(){
 
-        //         if(localStorage.getItem("vibrationPermission") === "true"){
-        //             navigator.vibrate(100);
-        //             console.log("vibrate");
-        //         }
+                if(localStorage.getItem("vibrationPermission") === "true"){
+                    navigator.vibrate(100);
+                    console.log("vibrate");
+                }
 
-        //         gameStatus = "Game On";
-        //         board = Array(9).fill("");
-        //         renderBoard();
-        //     });
+                gameStatus = "Game On";
+                board = Array(9).fill("");
+                renderBoard();
+            });
 
-        //     seeScore.addEventListener("change", function(){
+            seeScore.addEventListener("change", function(){
 
-        //         if(localStorage.getItem("vibrationPermission") === "true"){
-        //             navigator.vibrate(100);
-        //             console.log("vibrate");
-        //         }
+                if(localStorage.getItem("vibrationPermission") === "true"){
+                    navigator.vibrate(100);
+                    console.log("vibrate");
+                }
 
-        //         if(this.checked){
-        //             document.getElementById('scores').style.display = 'block';
-        //         } else {
-        //             document.getElementById('scores').style.display = 'none';
-        //         }
-        //     });
+                if(this.checked){
+                    document.getElementById('scores').style.display = 'block';
+                } else {
+                    document.getElementById('scores').style.display = 'none';
+                }
+            });
 
-        //     resetScore.addEventListener('click', function(){
-        //         document.getElementById('scoreX').value = 0;
-        //         document.getElementById("scoreO").value = 0;
+            resetScore.addEventListener('click', function(){
+                document.getElementById('scoreX').value = 0;
+                document.getElementById("scoreO").value = 0;
 
-        //         document.getElementById("dscoreO").innerHTML = 0;
-        //         document.getElementById("dscoreX").innerHTML = 0;
-        //     });
+                document.getElementById("dscoreO").innerHTML = 0;
+                document.getElementById("dscoreX").innerHTML = 0;
+            });
 
-        //     let score = JSON.parse(localStorage.scoreTicTacToe);
+            let score = JSON.parse(localStorage.scoreTicTacToe);
 
-        //     if (score['X'] !== undefined) {
-        //         document.getElementById('scoreX').value = score['X'];
-        //         document.getElementById('dscoreX').innerHTML = score['X'];
-        //     }
+            if (score['X'] !== undefined) {
+                document.getElementById('scoreX').value = score['X'];
+                document.getElementById('dscoreX').innerHTML = score['X'];
+            }
 
-        //     if (score['O'] !== undefined) {
-        //         document.getElementById('scoreO').value = score['O'];
-        //         document.getElementById('dscoreO').innerHTML = score['O'];
-        //     }
+            if (score['O'] !== undefined) {
+                document.getElementById('scoreO').value = score['O'];
+                document.getElementById('dscoreO').innerHTML = score['O'];
+            }
 
-        //     board = Array(9).fill("");
-        //     renderBoard();
-        // })
+            board = Array(9).fill("");
+            renderBoard();
+        })
         .catch(error => alert("Erreur dans le chargement de l'application."));
     },
 };
